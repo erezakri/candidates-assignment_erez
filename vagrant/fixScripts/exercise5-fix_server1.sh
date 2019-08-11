2caef770d11502917b04b81ad940fda6fa59ca63
erezakri <erezakri@gmail.com>
2019-08-11 17:45:41 +0300

    add deleted note

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#add fix to exercise5-server1 here
 echo "IdentityFile /vagrant/.vagrant/machines/server2/virtualbox/private_key" >> /etc/ssh/ssh_config
 echo "StrictHostKeyChecking no"  >> /etc/ssh/ssh_config
